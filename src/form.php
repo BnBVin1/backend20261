10b7bf96b55522c08a34e798a96867929155714e
BnBVin1 <168500070+BnBVin1@users.noreply.github.com>
2026-03-19 00:29:09 +0000

    aula 18/3/26

---
+++ src/form.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <h1>Form - 18/03/26</h1>
+        <form method="post" action="recebe.php">
+        Login<input type="text" id="Login" name="Login"><br>
+        Senha<input type="password" id="senha" name="senha"><br>
+        <input type="submit" value="validar">
+</form>
+
+</body>
+</html>
+
+
+
+
+
